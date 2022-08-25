83d3813a9a72cd0ffa3654e372039af1d21bd110
Parker Mitchell <Parker.Mitchell@guided-wave.com>
2022-08-25 13:48:50 -0700

    Add WPF Project with MVVM
    Adds our WPF user interface project using CommunityToolkit.Mvvm and implements dependencyinjection using Microsoft.Extensions.DependencyInjection

---
+++ PRMDesktopUI/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using PRMDesktopUI.Views;
+using PRMDesktopUI.ViewModels;
+
+namespace PRMDesktopUI
+{
+    public partial class App : Application
+    {
+        public static IHost? AppHost { get; set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<ShellView>();
+
+                    RegisterAllViewModels(services);
+                })
+                .Build();
+        }
+
+        private void RegisterAllViewModels(IServiceCollection services)
+        {
+            GetType().Assembly.GetTypes()
+                    .Where(type => type.IsClass && type.Name.EndsWith("ViewModel"))
+                    .ToList()
+                    .ForEach(viewModelType => services.AddTransient(viewModelType));
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var startupForm = AppHost.Services.GetRequiredService<ShellView>();
+            startupForm.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            startupForm.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
