641ebfac1222323b0b6a649d860ac70f4424fe4f
Parker Mitchell <Parker.Mitchell@guided-wave.com>
2022-08-25 16:30:20 -0700

    Added SQL Server Project with our initial tables

---
+++ TRMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(50) NOT NULL, 
+    [LastName] NCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
