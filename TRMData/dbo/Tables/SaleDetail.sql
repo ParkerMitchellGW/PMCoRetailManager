02905a8da14b3e10c9ee2e6cc7bbe9128a2ccaa7
Parker Mitchell <Parker.Mitchell@guided-wave.com>
2022-08-28 11:22:06 -0700

    added foreign keys to Inventoy, Sale, and SaleDetail tables

--- TRMData/dbo/Tables/SaleDetail.sql
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -6,5 +6,7 @@
     [Quantity] INT NOT NULL DEFAULT 1,
     [PurchasePrice] MONEY NOT NULL, 
     [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSaleTable] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+    CONSTRAINT [FK_SaleDetail_ToProductTable] FOREIGN KEY (ProductId) REFERENCES Product(Id), 
 
 )
