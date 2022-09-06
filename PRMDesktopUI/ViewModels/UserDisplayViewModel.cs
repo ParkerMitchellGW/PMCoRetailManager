5d37954721d6ac73a92a963f336258bc9f44b956
Parker Mitchell <Parker.Mitchell@guided-wave.com>
2022-09-06 13:45:16 -0700

    Add ability to add roles to users

--- PRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ PRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using PRMDesktopUI.Library.Api;
 using PRMDesktopUI.Library.Models;
@@ -21,7 +22,40 @@ namespace PRMDesktopUI.ViewModels
         private readonly IUserEndpoint _userEndpoint;
 
         [ObservableProperty]
-        public BindingList<UserModel> _users;
+        private BindingList<UserModel> _users = new();
+
+        [ObservableProperty]
+        private UserModel? _selectedUser;
+
+        [ObservableProperty]
+        private BindingList<string> _UserRoles = new();
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(RemoveSelectedRoleCommand))]
+        private string? _roleToRemove;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(AddSelectedRoleCommand))]
+        private string? _roleToAdd;
+
+        [ObservableProperty]
+        private BindingList<string> _availableRoles = new();
+
+
+        partial void OnSelectedUserChanged(UserModel? value)
+        {
+            if (SelectedUser is null)
+            {
+                UserRoles = new();
+            }
+            else
+            {
+                UserRoles = new(SelectedUser.Roles.Select(x => x.Value).ToList());
+                _ = LoadRoles();
+            }
+            AddSelectedRoleCommand.NotifyCanExecuteChanged();
+            RemoveSelectedRoleCommand.NotifyCanExecuteChanged();
+        }
 
         public UserDisplayViewModel(IStatusInfoDisplay statusInfo, IUserEndpoint userEndpoint)
         {
@@ -54,6 +88,44 @@ namespace PRMDesktopUI.ViewModels
         {
             var userList = await _userEndpoint.GetAll();
             Users = new(userList);
+        }
+
+        private bool CanAddSelectedRole => SelectedUser is not null && RoleToAdd is not null;
+
+        [RelayCommand(CanExecute = nameof(CanAddSelectedRole))]
+        private async void AddSelectedRole()
+        {
+            await _userEndpoint.AddUserToRole(SelectedUser!.Id, RoleToAdd!);
+
+            UserRoles.Add(RoleToAdd!);
+            AvailableRoles.Remove(RoleToAdd!);
+            OnPropertyChanged(nameof(Users));
+        }
+        private bool CanRemoveSelectedRole => SelectedUser is not null && RoleToRemove is not null;
+
+        [RelayCommand(CanExecute = nameof(CanRemoveSelectedRole))]
+        private async void RemoveSelectedRole()
+        {
+            await _userEndpoint.RemoveUserFromRole(SelectedUser!.Id, RoleToRemove!);
+
+            AvailableRoles.Add(RoleToRemove!);
+
+            UserRoles.Remove(RoleToRemove!);
+            OnPropertyChanged(nameof(Users));
+        }
+
+        private async Task LoadRoles()
+        {
+            RoleToAdd = null;
+            AvailableRoles.Clear();
+            var roles = await _userEndpoint.GetAllRoles();
+            foreach (var role in roles)
+            {
+                if (UserRoles.IndexOf(role.Value) == -1)
+                {
+                    AvailableRoles.Add(role.Value);
+                }
+            }
         }
     }
 }
