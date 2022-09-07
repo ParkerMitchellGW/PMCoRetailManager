47e1e399b5bdd4a056b8ccb09254be1d655e674d
Parker Mitchell <Parker.Mitchell@guided-wave.com>
2022-09-07 12:39:06 -0700

    upgraded API to asp.net core

---
+++ PRMApi/Controllers/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRMDataManager.Library.DataAccess;
+using PRMDataManager.Library.Models;
+
+namespace PRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        // Example of multiples roles being authorized
+        // Anyone with any of these roles can use this function
+        [Authorize(Roles = "Manager,Admin")]
+        public List<InventoryModel> GetInventory()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInventory();
+        }
+
+        // Example of needing multiple roles in order to authorize
+        // Must be ALL of these roles to use this function
+        //[Authorize(Roles = "WarehouseWorker")]
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+
+            data.SaveInventoryRecord(item);
+        }
+    }
+}
---
+++ PRMApi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRMDataManager.Library.DataAccess;
+using PRMDataManager.Library.Models;
+
+namespace PRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Cashier")]
+    public class ProductController : ControllerBase
+    {
+        // GET api/<controller>
+        public List<ProductModel> Get()
+        {
+            ProductData data = new ProductData();
+
+            return data.GetProducts();
+        }
+    }
+}
