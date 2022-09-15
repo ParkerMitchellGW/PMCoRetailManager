66395fb9a4ea471a9fc5b61ce02a0c6c606f7916
Parker Mitchell <Parker.Mitchell@guided-wave.com>
2022-09-15 15:40:59 -0700

    added a CreateUser function to UserEndpoint (and IUserEndpoint)

--- PRMDesktopUI.Library/Api/UserEndpoint.cs
+++ PRMDesktopUI.Library/Api/UserEndpoint.cs
@@ -31,7 +31,19 @@ namespace PRMDesktopUI.Library.Api
             }
         }
 
-        public async Task<Dictionary<string,string>> GetAllRoles()
+        public async Task CreateUser(CreateUserModel model)
+        {
+            var data = new { model.FirstName, model.LastName, model.EmailAddress, model.Password };
+
+            using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("/api/User/Register", data);
+
+            if (response.IsSuccessStatusCode == false)
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+
+        public async Task<Dictionary<string, string>> GetAllRoles()
         {
             using HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/User/Admin/GetAllRoles");
             if (response.IsSuccessStatusCode)
@@ -47,7 +59,7 @@ namespace PRMDesktopUI.Library.Api
 
         public async Task AddUserToRole(string userId, string roleName)
         {
-            var data = new {userId, roleName };
+            var data = new { userId, roleName };
 
             using HttpResponseMessage response = await _apiHelper.ApiClient.PostAsJsonAsync("/api/User/Admin/AddRole", data);
 
